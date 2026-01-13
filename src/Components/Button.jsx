e817fb85f0781a8ca4c96cc871a8c3e1b1440c0d
react-props <kumarrajeshnayak72@gmail.com>
2026-01-14 01:11:12 +0530

    Initial commit - React project without node_modules

---
+++ src/Components/Button.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Button = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default Button;
